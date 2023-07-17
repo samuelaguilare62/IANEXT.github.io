82b7198a42d1f52eaa14a88e11efa46fa198b8ae
charsdvp <carlossanchezdvp@gmail.com>
2023-07-16 20:28:59 -0600

    Significant changes to the structure and style of the webpage

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-hamburger = document.querySelector(".hamburger");
+hamburger = document.querySelector(".hamburger")
 hamburger.onclick = function(){
   navBar = document.querySelector(".nav-bar");
   navBar.classList.toggle("active")
